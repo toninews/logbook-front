e035d3a03869d22e9cf44ab79eb7f467a586a966
toninews <toninews57@gmail.com>
2026-01-23 15:49:09 -0300

    sync 2301 1549

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
         },
+          { rel: 'icon', type: 'public/svg', href: '/anchor.svg' }
       ],
     },
   },
